edd90b32bc53c60e584f55276d574ddb2a19a5c9
fzrsahi <fazrul.anugrah17@gmail.com>
2023-11-08 10:22:57 +0700

    feat : initializa bootsrap

---
+++ src/auth/Login.js
@@ -0,0 +1,9 @@
+import "./style/index.css";
+
+export function Login() {
+  return (
+    <div className="container">
+      <h1>Ini Login</h1>
+    </div>
+  );
+}
---
+++ src/auth/Register.js
@@ -0,0 +1,5 @@
+import "./style/index.css";
+
+export function Register() {
+  return <h1>Ini Register</h1>;
+}
